bf41b70329453ec3eeb403065af6e473f5ca181e
brayanismaelpr <brayanismaelpr@ufps.edu.co>
2022-10-20 22:55:08 -0500

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  View,
+  TouchableNativeFeedback,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableNativeFeedback onPress={() => Alert.alert("Soy una alerta")}>
+        <Text>Hola mundo!</Text>
+      </TouchableNativeFeedback>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
